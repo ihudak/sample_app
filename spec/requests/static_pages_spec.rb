cd079d2195b641b66cc8be59347ff4ef41d7f268
ihudak <ihudak@gmail.com>
2013-10-17 19:50:35 +0300

    refactored tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,32 +3,68 @@ require_relative '../spec_helper'
 describe "StaticPages" do
 
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_content(content) }
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
+  shared_examples_for "primary static pages" do | page_cont |
+    let(:content)    { page_cont }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_selector('title', text: "| #{content}") }
+  end
+
+  shared_examples_for "secondary static pages" do | page_cont |
+    let(:content)    { page_cont }
+    let(:heading)    { page_cont }
+    let(:page_title) { page_cont }
+
+    it_should_behave_like "all static pages"
+    it { should have_selector('title', text: content) }
+  end
+
   describe "Home Page" do
     before { visit root_path }
-    it { should     have_content('Home') }
-    it { should     have_selector('h1', text: 'Sample App') }
-    it { should     have_selector('title', text: full_title('')) }
-    it { should_not have_selector('title', text: '| Home') }
+    it_should_behave_like "primary static pages", 'Home'
   end
 
   describe "Help Page" do
     before { visit help_path }
-    it { should     have_content('Help') }
-    it { should     have_selector('h1', text: 'Help') }
-    it { should     have_selector('title', text: full_title('Help')) }
+    it_should_behave_like "secondary static pages", 'Help'
   end
 
   describe "About Page" do
     before { visit about_path }
-    it { should     have_content('About Us') }
-    it { should     have_selector('h1', text: 'About Us') }
-    it { should     have_selector('title', text: full_title('About Us')) }
+    it_should_behave_like "secondary static pages", 'About Us'
   end
 
   describe "Contacts Page" do
     before { visit contacts_path }
-    it { should     have_content('Contact Us') }
-    it { should     have_selector('h1', text: "Contact Us") }
-    it { should     have_selector('title', text: full_title('Contact Us')) }
+    it_should_behave_like "secondary static pages", 'Contact Us'
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    should have_selector 'title', text: full_title('')
+    should_not have_selector 'title', text: 'Home'
+    click_link "About"
+    should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    should have_selector 'title', text: full_title('Contact Us')
+    click_link "Home"
+    should have_selector 'title', text: full_title('')
+    should_not have_selector 'title', text: 'Home'
+    click_link "Sign up now!"
+    should have_selector 'title', text: 'Sign up'
+    click_link "sample app"
+    should have_selector 'title', text: full_title('')
+    should_not have_selector 'title', text: 'Home'
   end
 end
